732cd61426f3030017b2e98704ebda5761f56882
Mrack <Mrack@qq.com>
2021-11-15 08:39:07 +0800

    arm

--- graph.py
+++ graph.py
@@ -4,72 +4,97 @@ import graphviz
 import capstone
 from graphviz import Digraph
 
-cs = capstone.Cs(arch=capstone.CS_ARCH_X86, mode=capstone.CS_MODE_64)
-cs.detail = True
-with open("issue", mode="rb") as f:
-    bin1 = f.read()
 
-blocks = {}
+def draw(file, arch, mode):
+    cs = capstone.Cs(arch=arch, mode=mode)
+    cs.detail = True
+    with open(file, mode="rb") as f:
+        bin1 = f.read()
 
+    blocks = {}
 
-def disasm(data, address):
-    block = []
-    for ins in cs.disasm(data, address):
-        block.append(hex(ins.address) + " " + ins.mnemonic + " " + ins.op_str)
-        if ins.address in blocks:
-            block.pop()
-            return ins, "\l".join(block) + "\l", ins.address
-        if ins.mnemonic[0] == 'j':
-            return ins, "\l".join(block) + "\l", None
-        if ins.mnemonic == 'ret':
-            return ins, "\l".join(block) + "\l", None
+    def is_jcc(ins):
+        if arch == capstone.CS_ARCH_X86:
+            return ins.mnemonic[0] == 'j' and ins.mnemonic != 'jmp'
+        if arch == capstone.CS_ARCH_ARM64:
+            return ins.mnemonic[0:2] == 'b.'
 
-    return ins, "\l".join(block) + "\l", None
+    def is_cbz(ins):
+        return ins.mnemonic == "cbz"
 
+    def is_jmp(ins):
+        if arch == capstone.CS_ARCH_X86:
+            return ins.mnemonic == 'jmp'
+        if arch == capstone.CS_ARCH_ARM64:
+            return ins.mnemonic == 'b'
 
-start = 0x46B
-end = 0x51B
+    def disasm(data, address):
+        block = []
+        for ins in cs.disasm(data, address):
+            block.append(hex(ins.address) + " " + ins.mnemonic + " " + ins.op_str)
+            if ins.address in blocks:
+                block.pop()
+                return ins, "\l".join(block) + "\l", ins.address
+            if is_jcc(ins) or is_cbz(ins) or is_jmp(ins) or ins.mnemonic == 'ret':
+                return ins, "\l".join(block) + "\l", None
+        return ins, "\l".join(block) + "\l", None
 
-stack = []
-stack.append(start)
-while len(stack) != 0:
-    address = stack.pop()
-    if address in blocks and address in range(start, end):
-        continue
-    eip, block, rel = disasm(bin1[address:end], address)
-    blocks[address] = {"rel": [] if rel is None else [rel], "ins": block, "end": eip.address}
-    if eip is None:
-        continue
-    if eip.mnemonic == 'jmp':
-        if eip.operands[0].type == 1:
+    start = 0x0289C
+    end = 0x02E98
+
+    stack = []
+    stack.append(start)
+    while len(stack) != 0:
+        address = stack.pop()
+        if address in blocks and address in range(start, end):
             continue
-        blocks[address]["rel"].append(eip.operands[0].imm)
-        stack.append(eip.operands[0].imm)
-    elif eip.mnemonic[0] == 'j':
-        if eip.operands[0].type == 1:
+        eip, block, rel = disasm(bin1[address:end], address)
+        blocks[address] = {"rel": [] if rel is None else [rel], "ins": block, "end": eip.address}
+        if eip is None:
             continue
-        blocks[address]["rel"].append(eip.address + eip.size)
-        stack.append(eip.address + eip.size)
-        blocks[address]["rel"].append(eip.operands[0].imm)
-        stack.append(eip.operands[0].imm)
+        if is_jmp(eip):
+            if eip.operands[0].type == 1:
+                continue
+            blocks[address]["rel"].append(eip.operands[0].imm)
+            stack.append(eip.operands[0].imm)
+        elif is_cbz(eip):
+            blocks[address]["rel"].append(eip.address + eip.size)
+            stack.append(eip.address + eip.size)
+            if eip.operands[1].type == 1:
+                continue
+            blocks[address]["rel"].append(eip.operands[1].imm)
+            stack.append(eip.operands[1].imm)
+        elif is_jcc(eip):
+            blocks[address]["rel"].append(eip.address + eip.size)
+            stack.append(eip.address + eip.size)
+            if eip.operands[0].type == 1:
+                continue
+            blocks[address]["rel"].append(eip.operands[0].imm)
+            stack.append(eip.operands[0].imm)
 
-for b in blocks:
+
+    for b in blocks:
+        for k, v in blocks.items():
+            if k is not None and v["end"] is not None and b in range(k + 1, v["end"]):
+                v["ins"] = v["ins"][:v["ins"].index(hex(b))]
+                for i in range(0, len(v["rel"]) - 1):
+                    if v["rel"][i] > v["end"]:
+                        v["rel"][i] = None
+                if b not in v["rel"]:
+                    v["rel"].append(b)
+
+    dot = Digraph(comment='Graph')
+    dot.attr('node', shape='box')
+
     for k, v in blocks.items():
-        if k is not None and v["end"] is not None and b in range(k + 1, v["end"]):
-            v["ins"] = v["ins"][:v["ins"].index(hex(b))]
-            for i in range(0, len(v["rel"]) - 1):
-                if v["rel"][i] > v["end"]:
-                    v["rel"][i] = None
-            if b not in v["rel"]:
-                v["rel"].append(b)
+        dot.node(hex(k), v['ins'])
+        for i in v["rel"]:
+            if i is not None:
+                dot.edge(hex(k), hex(i), constraint='true')
 
-dot = Digraph(comment='Graph')
-dot.attr('node', shape='box')
+    dot.render(file + '.gv', view=True)
 
-for k, v in blocks.items():
-    dot.node(hex(k), v['ins'])
-    for i in v["rel"]:
-        if i is not None:
-            dot.edge(hex(k), hex(i), constraint='true')
 
-dot.render('round-table.gv', view=True)
+if __name__ == '__main__':
+    # draw("issue", capstone.CS_ARCH_X86, capstone.CS_MODE_64)
+    draw("libnative-lib.so", capstone.CS_ARCH_ARM64, capstone.CS_MODE_ARM)
